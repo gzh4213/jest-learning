68b0c3bc520525f62732fe01e4667002d2841c94
guozhen <guozhen4213@126.com>
2020-08-04 23:56:17 +0800

    Jest自动化测试样例

--- math.js
+++ math.js
@@ -5,3 +5,15 @@ function add(a, b) {
 function minus(a, b) {
   return a - b
 }
+
+function multi(a, b) {
+  return a * b
+}
+
+try {
+  module.exports = {
+    add,
+    minus,
+    multi
+  }
+} catch(e) {}
