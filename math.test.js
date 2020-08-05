ddb4340cf1802930d38c5b44fbd31e2edbb6752b
guozhen <guozhen4213@126.com>
2020-08-05 22:53:20 +0800

    jest 简单配置

--- math.test.js
+++ math.test.js
@@ -1,4 +1,4 @@
-const { add, minus, multi } = require('./math')
+import { add, minus, multi } from './math'
 
 test('测试方法 3 + 7', () => {
   expect(add(3,7)).toBe(10)
@@ -11,3 +11,10 @@ test('测试方法 3 - 3', () => {
 test('测试乘法 3 * 3', () => {
   expect(multi(3,3)).toBe(9)
 })
+
+// npm run test
+// jest (babel-jest)
+// babel-core
+// 取 .babelrc 配置
+// 在运行测试之前，结合babel，先把代码做一次转化
+// 运行转化过的测试用例代码
