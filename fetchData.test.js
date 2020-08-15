f73c4d6b570e6de39883e3adbcb8999ff8cd7a7e
guozhen <guozhen4213@126.com>
2020-08-15 23:13:59 +0800

    异步代码测试之resolves.toMatchObject

--- fetchData.test.js
+++ fetchData.test.js
@@ -1,18 +1,13 @@
 import { fetchData } from './fetchData'
 
-// test('fetchData 返回结果为 { success: true }', (done) => {
-//   fetchData(data => {
-//     expect(data).toEqual({
-//       success: true
-//     })
-//     done()
-//   })
-// })
-
 test('fetchData 返回结果为 { success: true }', () => {
-  return fetchData().then(res => {
-    expect(res.data).toEqual({
+  return expect(fetchData()).resolves.toMatchObject({
+    data: {
       success: true
-    })
+    }
   })
 })
+
+// test('fetchData 返回结果为 404', () => {
+//   return expect(fetchData()).rejects.toThrow()
+// })
