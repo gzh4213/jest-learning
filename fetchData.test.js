02395479e8b6b861efca364a8ee7a1e3e9aa3205
guozhen <guozhen4213@126.com>
2020-08-15 23:03:03 +0800

    异步代码测试方式2

--- fetchData.test.js
+++ fetchData.test.js
@@ -1,10 +1,18 @@
 import { fetchData } from './fetchData'
 
-test('fetchData 返回结果为 { success: true }', (done) => {
-  fetchData(data => {
-    expect(data).toEqual({
+// test('fetchData 返回结果为 { success: true }', (done) => {
+//   fetchData(data => {
+//     expect(data).toEqual({
+//       success: true
+//     })
+//     done()
+//   })
+// })
+
+test('fetchData 返回结果为 { success: true }', () => {
+  return fetchData().then(res => {
+    expect(res.data).toEqual({
       success: true
     })
-    done()
   })
 })
