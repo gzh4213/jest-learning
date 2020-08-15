4afbf270e2546df9d79385e2eed8308eaf2038ab
guozhen <guozhen4213@126.com>
2020-08-15 23:24:29 +0800

    异步代码测试

--- fetchData.test.js
+++ fetchData.test.js
@@ -1,13 +1,29 @@
 import { fetchData } from './fetchData'
 
-test('fetchData 返回结果为 { success: true }', () => {
-  return expect(fetchData()).resolves.toMatchObject({
-    data: {
-      success: true
-    }
-  })
-})
-
-// test('fetchData 返回结果为 404', () => {
-//   return expect(fetchData()).rejects.toThrow()
+// test('fetchData 返回结果为 { success: true }',async () => {
+//   await expect(fetchData()).resolves.toMatchObject({
+//     data: {
+//       success: true
+//     }
+//   })
 // })
+
+// test('fetchData 返回结果为 404', async () => {
+//   await expect(fetchData()).rejects.toThrow()
+// })
+
+// test('fetchData 返回结果为 { success: true }',async () => {
+//   const res =  await fetchData()
+//   expect(res.data).toEqual({
+//     success: true
+//   })
+// })
+
+test('fetchData 返回结果为 404', async () => {
+  expect.assertions(1)
+  try {
+    await fetchData()
+  } catch (error) {
+    expect(error.toString()).toEqual('Error: Request failed with status code 404')
+  }
+})
